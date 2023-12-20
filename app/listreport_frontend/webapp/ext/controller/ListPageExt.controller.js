21abf6040b1f9eb574653bdac112a3860a902165
Yo Ri <yo.ri@toppan.co.jp>
2023-12-20 05:04:15 +0000

    修改csv数据（为了好看）

--- app/listreport_frontend/webapp/ext/controller/ListPageExt.controller.js
+++ app/listreport_frontend/webapp/ext/controller/ListPageExt.controller.js
@@ -11,7 +11,7 @@ sap.ui.define(['sap/ui/core/mvc/ControllerExtension'], function (ControllerExten
 			 */
 			onInit: function () {
 				// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
-				var oModel = this.base.getExtensionAPI().getModel();
+				// var oModel = this.base.getExtensionAPI().getModel();
 				console.log("list page onInit");
 			}
 		}
